a65d20c3adbb5462c9ea6f07593c1a0bdbbefd3a
pezholio <pezholio@gmail.com>
2022-10-13 08:36:38 +0100

    Automatically add packages to the Brewfile

--- aliases.sh
+++ aliases.sh
@@ -3,3 +3,25 @@
 alias a="atom ."
 alias t="gittower ."
 alias c="code ."
+
+function bi() {
+  echo "==> Installing $1"
+  brew install "$@"
+  echo "==> Adding $1 to the Brewfile"
+  brew bundle dump --force
+  echo "==> Committing and pushing changes"
+  git add Brewfile
+  git commit -a -m "Adding $1 to the Brewfile"
+  git push
+}
+
+function bu() {
+  echo "==> Uninstalling $1"
+  brew install "$@"
+  echo "==> Removing $1 from the Brewfile"
+  brew bundle dump --force
+  echo "==> Committing and pushing changes"
+  git add Brewfile
+  git commit -a -m "Removing $1 from the Brewfile"
+  git push
+}
