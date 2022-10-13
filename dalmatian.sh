c8261d290f8b6f300ab8df467e457d3790534a02
pezholio <pezholio@gmail.com>
2022-10-13 08:27:12 +0100

    Ignore shellcheck source error

--- dalmatian.sh
+++ dalmatian.sh
@@ -3,4 +3,5 @@
 dalmatian_tools_path="$HOME/Documents/dalmatian-tools"
 export PATH="$PATH:$dalmatian_tools_path/bin"
 
+# shellcheck source=/dev/null
 source "$dalmatian_tools_path/support/bash-completion.sh"
